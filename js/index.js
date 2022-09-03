a80f0d67f9ed1ec4c841340a3c1ca079753ef1a4
Raj106292 <rajdas.106292@gmail.com>
2022-09-03 13:43:32 +0600

    modal added

--- js/index.js
+++ js/index.js
@@ -8,7 +8,7 @@ const loadCatagories = () => {
 const displayCatagories = (catagories) => {
     const catagoriesContainer = document.getElementById('catagory-container')
     catagories.forEach(catagory => {
-        console.log(catagory);
+        // console.log(catagory);
         const catagoryDiv = document.createElement('div');
         catagoryDiv.classList.add('col-lg-3', 'col-sm-6', 'fw-bold' , 'hoveron');
         catagoryDiv.innerHTML = `
@@ -21,17 +21,26 @@ const displayCatagories = (catagories) => {
 }
 
 const loadCatagoriesData = (catagoryId) => {
-    console.log('clicked');
+    // console.log('clicked');
+    toggleSpinner(true)
     const url = `https://openapi.programming-hero.com/api/news/category/${catagoryId}`;
     fetch(url)
     .then(resp => resp.json())
     .then(data => DisplayCatagoriesData(data.data))
 }
 const DisplayCatagoriesData = (catagories) => {
+    console.log(catagories);
     const catagoryDetailsContainer = document.getElementById('catagory-details');
     catagoryDetailsContainer.innerHTML = ' ';
+    if(catagories.length === 0){
+        catagoryDetailsContainer.innerText = 'No Data Found';
+    }
+    else{
+        catagoryDetailsContainer.innerText = catagories.length + ' Data Found';
+    }
+    toggleSpinner(false);
     catagories.forEach(catagory => {
-        console.log(catagory);
+        // console.log(catagory);
         const detailsDiv = document.createElement('div');
         detailsDiv.innerHTML=`
             <div class="card mb-3">
@@ -50,15 +59,15 @@ const DisplayCatagoriesData = (catagories) => {
                                                 <img src=${catagory.author.img} class="img-fluid rounded-circle" style="width: 50px">
                                             </div>
                                             <div class="ms-2">
-                                                <span class="d-block">${catagory.author.name}<span>
+                                                <span class="d-block">${catagory.author.name ? catagory.author.name : 'No data found'}<span>
                                                 <span class="d-block">${catagory.author.published_date}</span>
                                             </div>
                                         </div>
                                         <div>
-                                            <p><i class="fa-regular fa-eye"></i>${catagory.total_view}</p>
+                                            <p><i class="fa-regular fa-eye"></i>${catagory.total_view ? catagory.total_view : 'No View'}</p>
                                         </div>
                                         <div>
-                                            <button class="btn text-primary"><i class="fa-solid fa-arrow-right-long"></i></button>
+                                            <button onclick="loadDetailsNews('${catagory._id}')" class="btn text-primary" data-bs-toggle="modal" data-bs-target="#newsdetailmodal"><i class="fa-solid fa-arrow-right-long"></i></button>
                                         </div>
                                     </div>
                             </section>
@@ -70,5 +79,53 @@ const DisplayCatagoriesData = (catagories) => {
         catagoryDetailsContainer.appendChild(detailsDiv);
     })
 }
+
+const loadDetailsNews = (news_id) =>{
+    const url = `https://openapi.programming-hero.com/api/news/${news_id}`;
+    fetch(url)
+    .then(resp => resp.json())
+    .then(data => displayDetailsNews(data.data[0]))
+}
+
+const displayDetailsNews = (theNews) => {
+    console.log(theNews);
+    const newsDetailModal = document.getElementById('newsDetail');
+    newsDetailModal.innerHTML = `
+    <div class="modal-content">
+        <div class="modal-header">
+            <h5 class="modal-title" id="newsdetailmodalLabel">${theNews.title}</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+            <img src=${theNews.thumbnail_url}>
+            <p>${theNews.details}</p>
+            <div class="d-flex">
+                <img src=${theNews.author.img} class="rounded-circle" style="width:50px">
+                <div>
+                    <span class="d-block">Name: ${theNews.author.name ? theNews.author.name : 'No Data Found'}
+                    <span class="d-block">Time: ${theNews.author.published_date}
+                </div>
+            </div>
+            <div class="d-flex justify-content-between mt-3">
+                <p><i class="fa-regular fa-eye"></i>${theNews.total_view ? theNews.total_view : 'No view'}</p>
+                <p>ratings: ${theNews.rating.number}
+            <div>
+        </div>
+        <div class="modal-footer mt-5 text-end">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        </div>
+    </div>
+    `
+}
+
+const toggleSpinner = isLoading => {
+    const spinnerSection = document.getElementById('spinner');
+    if(isLoading){
+        spinnerSection.classList.remove('d-none');
+    }
+    else{
+        spinnerSection.classList.add('d-none');
+    }
+};
 
 loadCatagories();
